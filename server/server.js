d70db8b3c6d594408752f3c14cc14bc08d4b74dc
Hendrik Yong <hendrikyong1205@gmail.com>
2025-07-06 16:11:32 +0800

    Implement API endpoints for weather data retrieval and update server scripts

--- server/server.js
+++ server/server.js
@@ -1,16 +1,73 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.get('/api/weather', (req, res) => {
-  res.json({
-    location: "Singapore",
-    temperature: "31°C",
-    condition: "Sunny",
-  });
+app.get("/api/metadata", async (req, res) => {
+  const collectionId = "1456"; // NEA Collection ID
+  const url = `https://api-production.data.gov.sg/v2/public/api/collections/${collectionId}/metadata`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      return res.status(502).json({ error: "Failed to fetch data" });
+
+    const data = await response.json();
+    const childDatasets = data?.data?.collectionMetadata?.childDatasets || [];
+
+    // returns the dataset id for NEA collection 2 hour, 24 hour, 4 days
+    res.json({ childDatasets });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/api/2hour", async (req, res) => {
+  const url = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast";
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      return res.status(502).json({ error: "Failed to fetch data" });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/api/24hour", async (req, res) => {
+  const url = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast";
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      return res.status(502).json({ error: "Failed to fetch data" });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/api/4days", async (req, res) => {
+  const url = "https://api.data.gov.sg/v1/environment/4-day-weather-forecast";
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      return res.status(502).json({ error: "Failed to fetch data" });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
